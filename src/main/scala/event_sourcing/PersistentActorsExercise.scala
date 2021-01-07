634dc2ddc19c418a18f65381a1c3e1c6fa6b6f9d
aviadsTraiana <aviads@traiana.com>
2021-01-07 19:17:20 +0200

    improve voting system (persist only events (commands) )

--- src/main/scala/event_sourcing/PersistentActorsExercise.scala
+++ src/main/scala/event_sourcing/PersistentActorsExercise.scala
@@ -2,6 +2,9 @@ package event_sourcing
 
 import akka.actor.{ActorLogging, ActorSystem, Props}
 import akka.persistence.PersistentActor
+import sun.jvm.hotspot.HelloWorld.e
+
+import scala.:+
 
 object PersistentActorsExercise extends App {
 
@@ -17,43 +20,40 @@ object PersistentActorsExercise extends App {
   type NrVotes = Int
   case class Vote(citizenPID: PID, candidate: CandidateName)
   case object PrintResult
-  case class VoteSystemRecord(pid: PID,
-                              candidate: CandidateName,
-                              votesSoFar: Map[CandidateName, NrVotes],
-                              votedSoFar: List[PID])
+
 
   class VotingActor extends PersistentActor with ActorLogging {
     var voted: List[String] = List.empty
     var votes: Map[String, Int] = Map.empty
     override def receiveRecover: Receive = {
-      case VoteSystemRecord(pid, candidate, votesSoFar, votedSoFar) =>
-        voted = votedSoFar :+ pid
-        val newVoteCount = votesSoFar.getOrElse(candidate, 0) + 1
-        votes = votesSoFar + (candidate -> newVoteCount)
-        log.info(
-          s"vote event record with pid:$pid for candidate:$candidate restored")
+      case Vote(pid,candidate) =>
+        handleInternalState(candidate,pid)
+        log.info(s"vote event record with pid:$pid for candidate:$candidate restored")
 
     }
 
     override def receiveCommand: Receive = {
-      case Vote(pid, candidate) =>
+      case  vote @ Vote(pid, _) =>
         if (voted.contains(pid)) {
-          log.info(s"sorry $pid you already voted!")
+          log.warning(s"$pid already voted!")
         } else {
-          persist(
-            event = VoteSystemRecord(pid,
-                                     candidate,
-                                     votesSoFar = votes,
-                                     votedSoFar = voted)
-          ) { e =>
-            voted = e.votedSoFar :+ e.pid
-            val newVoteCount = e.votesSoFar.getOrElse(candidate, 0) + 1
-            votes = e.votesSoFar + (candidate -> newVoteCount)
-            log.info(s"${e.pid} your vote has been has been received :) ")
-          }
+          /*
+         1) create the event
+         2) persist the event
+         3) handle a state change after persisting is successful
+        */
+          persist(event = vote) // COMMAND sourcing
+          { e =>handleInternalState(e.candidate,pid = e.citizenPID)}
         }
       case PrintResult =>
         votes.foreach(vote => log.info(s"${vote._1} have ${vote._2} votes"))
+    }
+
+    private def handleInternalState(candidate: CandidateName, pid:PID): Unit = {
+      voted = voted :+ pid
+      val newVoteCount = votes.getOrElse(candidate, 0) + 1
+      votes = votes + (candidate -> newVoteCount)
+      log.info(s"$pid your vote has been has been received :) ")
     }
 
     override def persistenceId: String = "voting-actor"
@@ -62,11 +62,11 @@ object PersistentActorsExercise extends App {
   val system = ActorSystem("votingSystemDemo")
   val voteSystem = system.actorOf(Props[VotingActor], "voteSystemActor")
 //  voteSystem ! Vote("Aviad", "Pavel")
-//  voteSystem ! Vote("Aviad", "Pavel")
 //  voteSystem ! Vote("Aviv", "Aviad")
 //  voteSystem ! Vote("Pavel", "Aviad")
 //  voteSystem ! Vote("Irena", "Aviad")
 //  voteSystem ! Vote("Faiana", "Aviv")
+  voteSystem ! Vote("Aviad", "Pavel")
   voteSystem ! PrintResult
 
 }
